a54d90fad37314d71a6fa5fe71e0a2660b86dfc2
Anton Alexandrov <sunmeldino@gmail.com>
2020-06-23 16:59:55 +0300

    add arrays.py

---
+++ arrays.py
@@ -0,0 +1,13 @@
+import numpy as np
+a = np.array([1, 2, 3])
+print(a)
+b = np.array([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+    ], dtype=np.float)
+print(b)
+c = [1, 2, 3]
+print(c)
+g = None
+print(g)
